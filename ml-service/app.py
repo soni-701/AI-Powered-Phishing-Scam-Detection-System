fb25f363b304d3d008b56cb2d01103344d8a67fc
Soni Yadav <soniyadv7001@gmail.com>
2026-09-11 22:14:37 +0530

    Prepare ML service for deployment

--- ml-service/app.py
+++ ml-service/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
@@ -323,12 +325,10 @@ if __name__ == "__main__":
     print("")
 
 
-    app.run(
+port = int(os.environ.get("PORT", 8000))
 
-        host="0.0.0.0",
-
-        port=8000,
-
-        debug=True
-
-    )
+app.run(
+    host="0.0.0.0",
+    port=port,
+    debug=False
+)
